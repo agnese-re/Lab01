ee87c4af66ca5253e08a81ff2062b3edd0fc7e39
agnese-re <s271653@studenti.polito.it>
2022-03-25 15:36:01 +0100

    soluzione

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -12,7 +12,7 @@ import javafx.scene.control.TextField;
 
 public class FXMLController {
 	
-	Parole elenco ;
+	Parole elenco;	// oggetto di classe Parole su cui vengono richiamati i metodi 
 
     @FXML
     private ResourceBundle resources;
@@ -21,34 +21,86 @@ public class FXMLController {
     private URL location;
 
     @FXML
-    private TextField txtParola;
+    private TextField txtParola;	// campo TextField in cui e' stata inserita una nuova parola
 
     @FXML
-    private Button btnInserisci;
+    private Button btnInserisci;	// bottone per confermare l'inserimento di una parola in elenco
 
     @FXML
-    private TextArea txtResult;
+    private TextArea txtResult;		// campo TextArea in cui viene stampato elenco alfabetico
 
     @FXML
-    private Button btnReset;
+    private Button btnReset;		// bottone per resettare, quindi eliminare tutte le parole finora inserite
 
     @FXML
-    void doInsert(ActionEvent event) {
-    	// TODO
+    private Button btnCancella;		// bottone per cancellare da struttura dati la parola selezionata
+    
+    @FXML
+    private TextArea txtTime;		// campo TextArea in cui visualizzare i tempi di esecuzione di ciascuna operazione
+    
+    @FXML
+    void doInsert(ActionEvent event) {		// viene cliccato il bottone "Inserisci"
+    	String parolaInserita = txtParola.getText();	// parola inserita dall'utente
+    	
+    	/* Operazione #1: INSERT */
+    	double inizio = System.nanoTime();
+    	elenco.addParola(parolaInserita); 	// aggiungo parola nella struttura dati
+    	double fine = System.nanoTime();
+    	txtTime.clear(); 	// pulisco da eventuali tempi di operazioni precedenti
+    	txtTime.setText("Tempo impiegato INSERT: " + (fine-inizio)*1E-9 + " s");
+    	/* Fine sezione calcolo tempo/performance azione "Inserisci" */
+    	
+    	txtParola.clear(); 		// siamo pronti per inserire una nuova parola
+    	
+    	String stringaCompleta = "";
+    	for(String parola: elenco.getElenco())
+    		stringaCompleta += parola + "\n";
+    	txtResult.setText(stringaCompleta);
     }
 
     @FXML
     void doReset(ActionEvent event) {
-    	// TODO
+    	
+    	/* Operazione #2: RESET */
+    	double inizio = System.nanoTime();
+    	elenco.reset(); 		// elimino parole inserite dalla struttura dati
+    	double fine = System.nanoTime();
+    	txtTime.clear();
+    	txtTime.setText("Tempo impiegato RESET: " + (fine-inizio)*1E-9 + " s");
+    	/* Fine sezione calcolo tempo/performance azione "Reset" */
+    	
+    	txtResult.clear(); 		// pulisco la textArea da parole precedenti
     }
 
     @FXML
+    void doCancella(ActionEvent action) {
+    	String parolaDaCancellare = txtResult.getSelectedText();	// testo selezionato
+    	
+    	/* Operazione #3: DELETE */
+    	double inizio = System.nanoTime();
+    	elenco.cancellaParola(parolaDaCancellare);
+    	double fine = System.nanoTime();
+    	txtTime.clear();
+    	txtTime.setText("Tempo impiegato CANCELLA :" + (fine-inizio)*1E-9 + " s");
+    	/* Fine sezione calcolo tempo/performance azione "Cancella" */
+    	
+    	txtResult.clear(); 		// puliamo campo da eventuali precedenti parole
+    	String stringaCompleta = "";
+    	for(String parola: elenco.getElenco())
+    		stringaCompleta += parola + "\n";
+    	txtResult.setText(stringaCompleta);
+    }
+    
+    @FXML
     void initialize() {
+    	
         assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
-
-        elenco = new Parole() ;
+        assert btnCancella != null : "fx:id=\"btnCancella\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTime != null : "fx:id=\"txtTime\" was not injected: check your FXML file 'Scene.fxml'.";
+        
+        elenco = new Parole();
     }
 }
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,51 @@
 package it.polito.tdp.parole.model;
 
 import java.util.List;
+import java.util.*;
 
 public class Parole {
 		
+	private List<String> elencoParole;
+	
 	public Parole() {
-		//TODO
+		elencoParole = new LinkedList<String>();
+		// elencoParole = new ArrayList<String>();		// PERFORMANCE? MIGLIORI
+		/* 		***********************************		 */
+		
+		/*				   LINKEDLIST       ARRAYLIST    */
+		/* INSERT			3.0E-6 s	>	 1.2E-6 s	 */
+		/* RESET			5.3E-6 s	>	 3.6E-6 s    */
+		/* CANCELLA			1.2E-5 s	>	 7.3E-6 s    */
+		
+		/* 		***********************************		 */
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		elencoParole.add(p);
 	}
 	
+	// Ritorna elenco di parole finora inserite in ordine alfabetico
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		Collections.sort(elencoParole);	// sono stringhe. Ordinate naturalmente
+		return elencoParole;
 	}
+	
+	// Cancella la parola selezionata nell'area di testo. Cancellazione della SINGOLA parola
+	public void cancellaParola(String parolaDaCancellare) {
+	
+		boolean trovatoParola = false;	// la parola e' sicuramente nell'elenco. Sto selezionando da TextArea
+		int indiceParolaDaCancellare = 0;
+		for(int i = 0; i < elencoParole.size() && !trovatoParola; i++)
+			if(elencoParole.get(i).compareTo(parolaDaCancellare) == 0) {
+				trovatoParola = true;
+				indiceParolaDaCancellare = i;
+			}
+		elencoParole.remove(indiceParolaDaCancellare);
+		/* Oppure semplicemente elencoParole.remove(parolaDaCancellare); */
+		}
 	
 	public void reset() {
-		// TODO
+		elencoParole.clear();
 	}
 
 }
